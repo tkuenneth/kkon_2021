70e000fa6bf7955fd5aa485a10dbfff3e33c29a8
Thomas Kuenneth <thomas@thomaskuenneth.eu>
2021-04-18 13:41:21 +0200

    demo expanded

--- CoroutineDemo/app/src/main/java/com/thomaskuenneth/coroutinedemo/MainActivity.kt
+++ CoroutineDemo/app/src/main/java/com/thomaskuenneth/coroutinedemo/MainActivity.kt
@@ -2,12 +2,11 @@ package com.thomaskuenneth.coroutinedemo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
+// import androidx.lifecycle.lifecycleScope
 import com.thomaskuenneth.coroutinedemo.databinding.ActivityMainBinding
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.isActive
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,15 +17,46 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        lifecycleScope.launchWhenStarted {
-            var count = 0
-            while (isActive) {
-                val result = withContext(Dispatchers.IO) {
-                    delay(3000)
-                    count++
+//        lifecycleScope.launchWhenStarted {
+//            var count = 0
+//            while (isActive) {
+//                val result = withContext(Dispatchers.IO) {
+//                    delay(3000)
+//                    count++
+//                }
+//                binding.textview.text = "$result"
+//            }
+//        }
+
+        // don't do it like this :-)
+//        val job = GlobalScope.launch {
+//            var count = 0
+//            while (isActive) {
+//                delay(1000)
+//                println(count++)
+//            }
+//        }
+//        Handler(mainLooper).postDelayed({
+//            job.cancel()
+//        }, 10000)
+
+        val t = GlobalScope.launch {
+            val myFlow = flow {
+                for (i in listOf(10, 3, 42, 7)) {
+                    delay(5000)
+                    emit(i)
                 }
-                binding.textview.text = "$result"
+            }
+            myFlow.collect {
+                println("\n$it")
             }
         }
+        GlobalScope.launch {
+            while (t.isActive) {
+                println("-")
+                delay(1000)
+            }
+        }
+
     }
 }
